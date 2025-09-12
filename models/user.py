94bf9c54bb438ed1669e1780054c8af32881a556
Doonminus2 <piezapiezakok@gmail.com>
2025-09-12 15:31:12 +0700

    feat: add schma order + fix relation user table

--- models/user.py
+++ models/user.py
@@ -1,13 +1,16 @@
 
 from models import db
+from sqlalchemy.orm import relationship
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     role = db.Column(db.String(50), nullable=False)
+
+    orders = relationship("order", back_populates="user")
 
     def __repr__(self):
         return f'<User {self.email}>'
